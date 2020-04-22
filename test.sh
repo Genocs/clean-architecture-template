d8f9e2907c13cf3e47c4143c5e97773442b3944b
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2020-04-22 19:37:37 +0200

    comment old commands

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 rm -rf samples
 mkdir samples
 pushd samples
-dotnet new cleanarchitecture --use-cases full -n MyFull
-dotnet new cleanarchitecture --use-cases basic -n MyBasic
-dotnet new cleanarchitecture --use-cases readonly -n MyReadOnly
+dotnet new cleanarchitecture -n "MyCompany.MyProject"
+#dotnet new cleanarchitecture --use-cases basic -n MyBasic
+#dotnet new cleanarchitecture --use-cases readonly -n MyReadOnly
 popd
