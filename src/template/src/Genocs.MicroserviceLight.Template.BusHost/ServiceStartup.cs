522f193b9a577c8d648b8e91a20393824cc1cccb
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2021-03-20 21:53:01 +0100

    Added check for enterprise service bus and persistence layer

--- src/template/src/Genocs.MicroserviceLight.Template.BusHost/ServiceStartup.cs
+++ src/template/src/Genocs.MicroserviceLight.Template.BusHost/ServiceStartup.cs
@@ -25,9 +25,9 @@ namespace Genocs.MicroserviceLight.Template.BusHost
             services.AddApplicationInsightsKubernetesEnricher();
             services.AddApplicationInsightsTelemetry(context.Configuration);
 
-            services.Configure<Infrastructure.ServiceBus.AzureServiceBusOptions>(context.Configuration.GetSection("AzureServiceBusSettings"));
-            services.Configure<Infrastructure.ServiceBus.RebusBusOptions>(context.Configuration.GetSection("RebusBusSettings"));
             services.Configure<Infrastructure.ServiceBus.ParticularServiceBusOptions>(context.Configuration.GetSection("ParticularServiceBusSettings"));
+            //services.Configure<Infrastructure.ServiceBus.AzureServiceBusOptions>(context.Configuration.GetSection("AzureServiceBusSettings"));
+            //services.Configure<Infrastructure.ServiceBus.RebusBusOptions>(context.Configuration.GetSection("RebusBusSettings"));
 
             // The HostService 
             // This is the Service entry point management
