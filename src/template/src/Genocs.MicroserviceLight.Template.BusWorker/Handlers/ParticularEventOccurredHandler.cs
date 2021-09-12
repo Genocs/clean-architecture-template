5f09b1659e40071a89e97b6558a1e4a04443955d
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2021-09-12 16:11:30 +0200

    cleanup data

--- src/template/src/Genocs.MicroserviceLight.Template.BusWorker/Handlers/ParticularEventOccurredHandler.cs
+++ src/template/src/Genocs.MicroserviceLight.Template.BusWorker/Handlers/ParticularEventOccurredHandler.cs
@@ -1,9 +1,14 @@
-﻿using NServiceBus;
-using NServiceBus.Logging;
-using System.Threading.Tasks;
-
-namespace Genocs.MicroserviceLight.Template.BusWorker.Handlers
+﻿namespace Genocs.MicroserviceLight.Template.BusWorker.Handlers
 {
+    using ParticularShared.TransactionSaga;
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This event handler will be registerd automatcally. It do not need any action to be up and running 
+    /// </summary>
     public class ParticularEventOccurredHandler : IHandleMessages<Shared.Events.RegistrationCompleted>
     {
         private readonly ILog _logger = LogManager.GetLogger<ParticularEventOccurredHandler>();
@@ -13,19 +18,23 @@ namespace Genocs.MicroserviceLight.Template.BusWorker.Handlers
         //public ParticularEventOccurredHandler(ILog logger)
         //    => _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
 
-        public Task Handle(Shared.Events.RegistrationCompleted message, IMessageHandlerContext context)
+        public async Task Handle(Shared.Events.RegistrationCompleted message, IMessageHandlerContext context)
         {
 
             _logger.Info($"RegistrationCompleted on CustomerId: '{message.CustomerId}', AccountId: '{message.AccountId}', ");
+
+            // Start the saga
+            await context.SendLocal(new TransactionLoaded()
+            {
+                RequestId = Guid.NewGuid().ToString(),
+                TransactionId = Guid.NewGuid().ToString()
+            });
 
             // Remove the comments to simulate some exception
             //if(counter++ < 10 )
             //{
             //    throw new InvalidOperationException($"exception number: '{counter}'");
             //}
-
-            // Do something with the message here
-            return Task.CompletedTask;
         }
 
     }
---
+++ src/template/src/Genocs.MicroserviceLight.Template.Shared.Particular/IntegrationEvents/BasicEvent.cs
@@ -0,0 +1,8 @@
+﻿using NServiceBus;
+
+namespace Genocs.MicroserviceLight.Template.Shared.Particular.IntegrationEvents
+{
+    public class BasicEvent : IEvent
+    {
+    }
+}
