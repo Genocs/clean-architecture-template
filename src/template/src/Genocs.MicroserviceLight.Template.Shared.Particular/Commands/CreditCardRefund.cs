8af6750036171259710f4542dc4af9f186cef6dc
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2021-09-12 13:27:05 +0200

    Changed Naming convention

--- src/template/src/Genocs.MicroserviceLight.Template.Shared.Particular/Commands/CreditCardRefund.cs
+++ src/template/src/Genocs.MicroserviceLight.Template.Shared.Particular/Commands/CreditCardRefund.cs
@@ -2,15 +2,7 @@
 
 namespace Genocs.MicroservicesLight.Template.ParticularShared.Commands
 {
-    public class TransactionLoaded : ICommand
-    {
-        public string TransactionId { get; set; }
-    }
 
-    public class TransactionUnloaded : ICommand
-    {
-        public string TransactionId { get; set; }
-    }
 
     public class FeeUnloaded : ICommand
     {
